d44a2c88855ba1345f5c47511dfb35f89ecce491
Lehmaning <60032845+Lehmaning@users.noreply.github.com>
2022-01-01 16:48:09 +0800

    Git init

---
+++ switch.lua
@@ -0,0 +1,10 @@
+function switch.switch(...)
+				frame = frame or {}
+				s = {}
+				local args = table.pack(...)
+				for k,v in pairs(args) do
+								s[i] = k and v
+    end
+				return s
+end
+return switch
