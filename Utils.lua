5e7d0fc48fc09c4953a0778b73d2443cd6d427d1
Lehmaning <60032845+Lehmaning@users.noreply.github.com>
2022-01-04 02:31:42 +0800

    Revert "First release."
    This reverts commit aa68179de471cf4d4e5d8385d50d587ad1233d31.

--- Utils.lua
+++ Utils.lua
@@ -9,36 +9,12 @@ function u.isInArray(val, t)
 	return false
 end
 
-function u.switch(args, input)
-	input = input or true
+function u.switch(args)
 	for k,v in pairs(args) do
-		if k==input then
+		if k==true then
 			return v
 		end
 	end
 end
 
---[=[
-function html:addTag(...)
-	local args, tagName, wikitext, className,attr, style, parent
-	if #{...} == 1 then
-        args = ...
-		tagName = args.tagName
-		wikitext = args.wikitext
-		className = args.className
-		attr = args.attr
-		style = args.style
-	else
-		tagName, wikitext, className, attr, style, parent = ...
-	end
-	local output = self:tag(tagName):wikitext(wikitext)
-	if attr then output = output:attr(attr) end
-	if className then output = output:addClass(className) end
-	if style then output = output:cssText(style) end
-	parent = output
-	output.addTag = function() return html:addTag(tagName, wikitext, className, attr, style, parent) end
-	output.done = function() return parent or self end
-	return output--:done
-end
---]=]
 return u
