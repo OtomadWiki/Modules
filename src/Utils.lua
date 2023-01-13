6d0741e0d711d267d761c702c1ecc30481558284
Lehmaning <60032845+Lehmaning@users.noreply.github.com>
2023-01-13 20:55:15 +0800

    style: remove unuseful comments

--- src/Utils.lua
+++ src/Utils.lua
@@ -1,53 +1,30 @@
 local u = {}
 
 function u.isInArray(val, t)
-        for _, v in ipairs(t) do
-                if v == val then
-                        return true
-                end
-        end
-        return false
+  for _, v in ipairs(t) do
+    if v == val then
+      return true
+    end
+  end
+  return false
 end
 
 function u.isInDict(val, t)
-        for k, v in pairs(t) do
-                if v == val or k == val then
-                        return true
-                end
-        end
-        return false
+  for k, v in pairs(t) do
+    if v == val or k == val then
+      return true
+    end
+  end
+  return false
 end
 
 function u.switch(self, args)
-        local input = self or true
-        for k,v in pairs(args) do
-                if k==input then
-                        if k then return v end
-                end
-        end
+  local input = self or true
+  for k,v in pairs(args) do
+    if k==input then
+      if k then return v end
+    end
+  end
 end
 
---[=[
-function html:addTag(...)
-        local args, tagName, wikitext, className,attr, style, parent
-        if #{...} == 1 then
-        args = ...
-                tagName = args.tagName
-                wikitext = args.wikitext
-                className = args.className
-                attr = args.attr
-                style = args.style
-        else
-                tagName, wikitext, className, attr, style, parent = ...
-        end
-        local output = self:tag(tagName):wikitext(wikitext)
-        if attr then output = output:attr(attr) end
-        if className then output = output:addClass(className) end
-        if style then output = output:cssText(style) end
-        parent = output
-        output.addTag = function() return html:addTag(tagName, wikitext, className, attr, style, parent) end
-        output.done = function() return parent or self end
-        return output--:done
-end
---]=]
 return u
